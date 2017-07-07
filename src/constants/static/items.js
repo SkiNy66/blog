79fd62d884212731f786b0396b16376a697dfd00
Ilya Timchenko <skiny66.ilya@gmail.com>
2017-07-08 01:35:37 +0600

    lesson 4

---
+++ src/constants/static/items.js
@@ -0,0 +1,44 @@
+export const items = [
+  {
+    image: {
+      src: 'http://l7.alamy.com/zooms/b5ebab7ac1114104b4318416a3ef5bd2/the-state-hermitage-museum-the-knights-hall-knights-in-armor-on-horseback-fxyg2x.jpg',
+      width: '50px',
+      height: '50px',
+      alt: 'pic'
+    },
+    id: '1',
+    text: 'в патруле))',
+    author: 'Рыцарь',
+    createdAt: '0089-01-23',
+    updatedAt: '1379-03-17',
+    count: 3
+  },
+  {
+    image: {
+      src: 'https://goo.gl/TrkQOh',
+      width: '50px',
+      height: '50px',
+      alt: 'pic'
+    },
+    id: '2',
+    text: 'в таверне))',
+    author: 'Рыцарь',
+    createdAt: '0089-01-23',
+    updatedAt: '1379-03-17',
+    count: 56
+  },
+  {
+    image: {
+      src: 'https://goo.gl/TrkQOh',
+      width: '50px',
+      height: '50px',
+      alt: 'pic'
+    },
+    id: '3',
+    text: 'на курорте))',
+    author: 'Рыцарь',
+    createdAt: '0089-01-23',
+    updatedAt: '1379-03-17',
+    count: 13
+  }
+];
